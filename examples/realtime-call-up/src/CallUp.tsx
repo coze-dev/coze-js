6b8ad37c2a05b9a68834653f42ae9ded28d6e3c8
chyroc <chyroc@qq.com>
2024-11-07 00:08:24 +0800

    x

--- examples/realtime-call-up/src/CallUp.tsx
+++ examples/realtime-call-up/src/CallUp.tsx
@@ -2,8 +2,11 @@ import { OAuthToken, type SimpleBot } from '@coze/api';
 import { RealtimeClient } from '@coze/realtime-api';
 import React, { useState, useEffect, useRef } from 'react';
 import useCozeAPI, { INVALID_ACCESS_TOKEN, VoiceOption } from './use-coze-api';
-// TODO: fix missing coze.png asset
 import cozeLogo from './assets/coze.png';
+import phoneIcon from './assets/phone.svg';
+import closeIcon from './assets/close.svg';
+import microphoneIcon from './assets/microphone.svg';
+import microphoneOffIcon from './assets/microphone-off.svg';
 import './CallUp.css';
 
 const CallUp: React.FC = () => {
@@ -256,16 +259,11 @@ const CallUp: React.FC = () => {
               className={`mute-button ${isMuted ? 'muted' : ''}`}
               onClick={handleToggleMicrophone}
             >
-              <svg
+              <img
+                src={isMuted ? microphoneOffIcon : microphoneIcon}
                 className={`microphone-icon ${isMuted ? 'muted' : ''}`}
-                viewBox="0 0 24 24"
-              >
-                <path d="M12 14c1.66 0 3-1.34 3-3V5c0-1.66-1.34-3-3-3S9 3.34 9 5v6c0 1.66 1.34 3 3 3zm-1-9c0-.55.45-1 1-1s1 .45 1 1v6c0 .55-.45 1-1 1s-1-.45-1-1V5z" />
-                <path d="M17 11c0 2.76-2.24 5-5 5s-5-2.24-5-5H5c0 3.53 2.61 6.43 6 6.92V21h2v-3.08c3.39-.49 6-3.39 6-6.92h-2z" />
-                {isMuted && (
-                  <path d="M3.27 3L2 4.27l4.19 4.19L6 9c0 3.53 2.61 6.43 6 6.92V19h2v-3.08c.57-.08 1.12-.24 1.64-.45L19.73 21 21 19.73 3.27 3z" />
-                )}
-              </svg>
+                alt="microphone"
+              />
             </button>
           )}
           <button
@@ -273,13 +271,13 @@ const CallUp: React.FC = () => {
             onClick={handleCall}
           >
             {isCallActive ? (
-              <svg className="end-call-icon-svg" viewBox="0 0 24 24">
-                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
-              </svg>
+              <img
+                src={closeIcon}
+                className="end-call-icon-svg"
+                alt="end call"
+              />
             ) : (
-              <svg className="call-icon-svg" viewBox="0 0 24 24">
-                <path d="M20.01 15.38c-1.23 0-2.42-.2-3.53-.56-.35-.12-.74-.03-1.01.24l-1.57 1.97c-2.83-1.35-5.48-3.9-6.89-6.83l1.95-1.66c.27-.28.35-.67.24-1.02-.37-1.11-.56-2.3-.56-3.53 0-.54-.45-.99-.99-.99H4.19C3.65 3 3 3.24 3 3.99 3 13.28 10.73 21 20.01 21c.71 0 .99-.63.99-1.18v-3.45c0-.54-.45-.99-.99-.99z" />
-              </svg>
+              <img src={phoneIcon} className="call-icon-svg" alt="start call" />
             )}
           </button>
         </div>
