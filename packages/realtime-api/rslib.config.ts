2394e8a82ab2587dde2c0c775b30e838f89af429
wfc <wangfocheng@bytedance.com>
2024-11-13 16:52:52 +0800

    feat: rslib
    feat: rslib config and package.json
    
    feat: add umd and types
    
    feat: coze-js use rslib
    
    feat: libs map

---
+++ packages/realtime-api/rslib.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, type LibConfig } from '@rslib/core';
+
+function getLibShared(format: LibConfig['format']) {
+  const shared: LibConfig = {
+    output: {
+      distPath: {
+        root: `./dist/${format}`,
+      },
+    },
+    format,
+    syntax: 'es6',
+    dts: true,
+  };
+  return shared;
+}
+
+export default defineConfig({
+  source: {
+    tsconfigPath: './tsconfig.build.json',
+  },
+  output: {
+    target: 'web',
+  },
+  lib: ['esm', 'cjs', 'umd'].map(r => getLibShared(r as LibConfig['format'])),
+});
