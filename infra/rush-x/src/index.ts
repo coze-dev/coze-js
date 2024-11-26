4860c8c6fbe63509af296ab10064ecbc9573c0a3
wenming.2020 <wenming.2020@bytedance.com>
2024-11-27 06:11:25 +0800

    test: 测试效果

--- infra/rush-x/src/index.ts
+++ infra/rush-x/src/index.ts
@@ -4,6 +4,8 @@ import { logger } from '@coze-infra/rush-logger';
 
 import { IncrementAction } from './actions';
 
+const a = 'foo';
+
 export class RushCICommandLine extends CommandLineParser {
   readonly rushConfiguration!: RushConfiguration;
 
