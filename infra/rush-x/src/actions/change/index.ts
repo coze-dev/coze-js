44fa2cdf5b1883b4105a688a0066bd2689754469
fanwenjie <fanwenjie.fe@bytedance.com>
2024-12-10 18:20:24 +0800

    chore: setup git hook
    chore: trigger change and should be revert
    
    Revert "chore: trigger change and should be revert"
    
    This reverts commit b9bdd02a5b306012221aa0e7e008d9a1c05eb3d4.
    
    chore: setup git hook
    
    chore: trigger change and should be revert
    
    chore: setup git hook
    
    chore: setup git hook
    
    chore: trigger change and should be revert
    
    Revert "chore: trigger change and should be revert"
    
    This reverts commit 9da1a55f58d0eb73120854c414246ead4a662ecd.
    
    chore: trigger change and should be revert
    
    Revert "chore: trigger change and should be revert"
    
    This reverts commit 91308f8cd7e35190bc60d8493d8b7aae6bd6f2c1.

--- infra/rush-x/src/actions/change/index.ts
+++ infra/rush-x/src/actions/change/index.ts
@@ -9,12 +9,12 @@ export const installAction: InstallAction = (program: Command) => {
     .command('change')
     .description('Generate changes in a simple way.')
     .option(
-      '-c, --commit-msg <commit-msg>',
+      '-c, --commit-msg <string>',
       '本次提交信息,默认读取 .git/COMMIT_EDITMSG',
     )
-    .option('-a, --amend-commit <boolean>', '是否 amend commit 阶段')
-    .option('-i, --ci <boolean>', '是否在 CI 环境')
-    .action((options: ChangeOptions) => {
-      generateChangeFiles(options);
+    .option('-a, --amend-commit', '是否 amend commit 阶段')
+    .option('-i, --ci', '是否在 CI 环境')
+    .action(async (options: ChangeOptions) => {
+      await generateChangeFiles({ ...options });
     });
 };
