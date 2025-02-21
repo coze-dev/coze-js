dca137c59333dec55bae1bab049e7a7c6d46d11b
gaoding.devingao <gaoding.devingao@bytedance.com>
2025-02-21 17:27:30 +0800

    feat: 修改覆盖率范围

--- packages/chat-sdk/vitest.config.ts
+++ packages/chat-sdk/vitest.config.ts
@@ -8,6 +8,20 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['test/**/*.test.{js,jsx,ts,tsx}'],
+    coverage: {
+      include: ['src/libs/utils/*.{ts,jsx,js,tsx}'],
+      exclude: [
+        'src/libs/utils/index.ts',
+        'src/libs/utils/mini-coze-api.ts',
+        'src/libs/utils/modal.ts',
+        'src/libs/utils/webm-to-wav.ts',
+        'src/libs/utils/safe-json-parse.ts',
+        'src/libs/utils/play-audio.ts',
+        'src/libs/utils/decorder.ts',
+        'src/libs/utils/get-svg-base64.ts',
+        'src/libs/utils/toast.ts',
+      ],
+    },
     server: {
       deps: {
         inline: ['@tarojs/runtime'],
