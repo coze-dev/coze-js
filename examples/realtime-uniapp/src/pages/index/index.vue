f5cc641ca753b941325c0f30ea310235e192d1b6
shenxiaojie.316 <shenxiaojie.316@bytedance.com>
2025-03-06 11:36:43 +0800

    feat(uniapp): Add webview

--- examples/realtime-uniapp/src/pages/index/index.vue
+++ examples/realtime-uniapp/src/pages/index/index.vue
@@ -1,41 +1,16 @@
 <template>
   <view class="content">
-    <image class="logo" src="/static/logo.png" />
-    <view class="text-area">
-      <text class="title">{{ title }}</text>
-    </view>
+    <web-view src="http://localhost:3000"></web-view>
   </view>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
-const title = ref('Hello')
+// No script needed for this simple WebView
 </script>
 
 <style>
 .content {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-}
-
-.logo {
-  height: 200rpx;
-  width: 200rpx;
-  margin-top: 200rpx;
-  margin-left: auto;
-  margin-right: auto;
-  margin-bottom: 50rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
+  width: 100%;
+  height: 100vh;
 }
 </style>
