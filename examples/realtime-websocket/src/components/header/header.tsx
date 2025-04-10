57df7a783adc6a3aeac815eafc0ae67d38414a66
shenxiaojie.316 <shenxiaojie.316@bytedance.com>
2025-04-10 11:00:35 +0800

    refactor: redesign UI with Ant Design layout system

--- examples/realtime-websocket/src/components/header/header.tsx
+++ examples/realtime-websocket/src/components/header/header.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
-import { Layout, Button } from 'antd';
-import { GithubOutlined } from '@ant-design/icons';
+import { Layout } from 'antd';
 
 import './header.css';
 import logo from '../../logo.svg';
@@ -28,14 +27,6 @@ const AppHeader: React.FC<AppHeaderProps> = ({
         onSettingsChange={onSettingsChange}
         localStorageKey={localStorageKey}
       />
-      <Button
-        type="link"
-        icon={<GithubOutlined />}
-        href="https://github.com/coze-dev/coze-js/tree/main/examples/realtime-websocket"
-        target="_blank"
-      >
-        Github
-      </Button>
     </div>
   </Header>
 );
